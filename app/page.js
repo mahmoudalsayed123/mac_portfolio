cd7cd9ef07d3adbf6d2cd09625a1339a037b809c
mahmoudalsayed123 <mahmoudalsayed05789@gmail.com>
2026-02-19 03:59:31 +0200

    fix design bug

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 function animateText(text, defaultSize = "30px", hoverSize = "100px") {
   return text.split("").map((letter, index) => (
     <motion.span
-      className=" font-bold text-white italic font-sans tracking-wider cursor-pointer"
+      className="hidden md:block font-bold text-white italic font-sans tracking-wider cursor-pointer"
       key={index}
       initial={{ fontSize: defaultSize }}
       whileHover={{ fontSize: hoverSize }}
@@ -22,11 +22,14 @@ const HomePage = () => {
         alt="wallpaper"
         className="w-full h-screen object-cover"
       />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+      <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
         {animateText("Hey I'm Mahmoud Welcom To My", "30px", "50px")}
         <br />
         {animateText("Portfolio.", "80px", "100px")}
       </div>
+      <h1 className="text-[100px] font-bold font-sans">
+        These Portfolio Enable Only On Desktop Screen
+      </h1>
     </main>
   );
 };
